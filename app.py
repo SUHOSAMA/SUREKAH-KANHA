48d0277dc6e5a9ed1712574bdce129daa5708e08
SUHOSAMA <132054328+SUHOSAMA@users.noreply.github.com>
2023-04-28 14:59:30 +0000

    Changes with HTML,CSS and Flask

--- app.py
+++ app.py
@@ -2,10 +2,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [{
+  'id': 2,
+  'title': 'data scientist',
+  'location': 'usa',
+  'salary': "Rs. 10,0000"
+}, {
+  'id': 2,
+  'title': 'data scientist',
+  'location': 'England',
+  'salary': "Rs. 19,0000"
+}, {
+  'id': 3,
+  'title': 'frontend engineer',
+  'location': 'canada',
+  'salary': "Rs. 15,0000"
+}, {
+  'id': 4,
+  'title': 'backend engineer',
+  'location': 'london',
+  'salary': "Rs. 14,0000"
+}]
+
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name='Jovian')
 
 
 if __name__ == "__main__":
